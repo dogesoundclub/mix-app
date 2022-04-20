20017af32b4e30db840fa0ac7661776c1941e0b9
dilrong <dilrong@dilrong.com>
2022-04-21 00:07:08 +0900

    fix: Prompt

--- src/component/shared/dialogue/Prompt.ts
+++ src/component/shared/dialogue/Prompt.ts
@@ -14,16 +14,19 @@ export default class Prompt extends Popup {
         super(".popup-background");
         this.append(
             this.content = el(".dialogue.prompt",
+                el(".close-container", { click: () => this.delete(), },
+                    el("img", { src: "/images/shared/icn/close.svg", alt: "close" }),
+                ),
                 el("h2", title),
                 el("p", message),
                 el(".input-container",
                     this.input = el("input"),
                 ),
                 el(".button-container",
-                    el("button", "취소", {
+                    el("button.left", "취소", {
                         click: () => this.delete(),
                     }),
-                    el("button", confirmTitle, {
+                    el("button.right", confirmTitle, {
                         click: () => {
                             confirm(this.input.domElement.value);
                             this.delete();
