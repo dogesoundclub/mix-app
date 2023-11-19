2ac63b2e9221f925e35f612d729afc12c63490bb
DSC-LABEL <dogesoundclub@gmail.com>
2023-11-20 00:48:20 +0900

    changed websocketprovider to httprovider

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -2,14 +2,7 @@ import { BigNumber } from "@ethersproject/bignumber";
 
 class Klaytn {
 
-    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("https://klaytn-mainnet-rpc.allthatnode.com:8551", {
-        reconnect: {
-            auto: true,
-            delay: 1000,
-            maxAttempts: true,
-            onTimeout: false
-        },
-    }));
+    private caver = new (window as any).Caver(new (window as any).Caver.providers.HttpProvider("https://klaytn-mainnet-rpc.allthatnode.com:8551"));
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
